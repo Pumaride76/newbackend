9d8994b2cedd97d636bf3788c84b698da23c2d11
aminevtible <iminevitible9945@gmail.com>
2024-01-18 19:36:18 +0530

    new

--- app.js
+++ app.js
@@ -7,6 +7,10 @@ app.get("/",(req,res)=>{
     res.send("hello world")
 })
 
+app.get('/create',(req,res)=>{
+    res.send("hi bitch")
+})
+
 app.listen(process.env.PORT ,()=>{
     console.log(`server is running at ${port}`)
 })
